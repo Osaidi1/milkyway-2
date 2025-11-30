0ec307f1e1113915939b410f87b1f803af6c59f1
Osaidi1 <hassanosaid123@gmail.com>
2025-11-30 14:28:27 +0100

    finnaly found the error in the weapon positioning system, rotation instead of rotation_degress, now also added weapon moving delayed with camera now going to add realism to weapon like idle bobbing and functionality

--- scripts/weapon_resource.gd
+++ scripts/weapon_resource.gd
@@ -5,6 +5,12 @@ extends Resource
 @export_category("Orientation")
 @export var position: Vector3
 @export var rotation: Vector3
+@export_category("Weapon Sway")
+@export var min := Vector2(-20.0, -20.0)
+@export var max := Vector2(20.0, 20.0)
+@export_range(0, 0.2, 0.01) var speed_position := 0.07
+@export_range(0, 0.2, 0.01) var speed_rotation := 0.1
+@export_range(0, 0.25, 0.01) var amount_position := 0.1
+@export_range(0, 50, 0.1) var amount_rotation := 30.0
 @export_category("Visuals")
 @export var mesh_scene: Mesh
-@export var shadow: bool
--- scripts/weapons_manager.gd
+++ scripts/weapons_manager.gd
@@ -8,10 +8,14 @@ extends Node3D
 		if Engine.is_editor_hint():
 			load_weapon()
 
-@onready var model: MeshInstance3D = $Model
+var mouse_movement: Vector2
 
 func _ready() -> void:
+	await owner.ready
 	load_weapon()
+
+func _process(delta: float) -> void:
+	weapon_sway(delta)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("weapon_up"):
@@ -20,12 +24,18 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("weapon_down"):
 		weapon = load("res://weapon_resource/crowbar.tres")
 		load_weapon()
+	if event is InputEventMouseMotion:
+		mouse_movement = event.relative
 
 func load_weapon() -> void:
-	model.mesh = weapon.mesh_scene
+	self.mesh = weapon.mesh_scene
 	position = weapon.position
-	rotation = weapon.rotation
-	if weapon.shadow:
-		model.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_ON
-	else:
-		model.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	rotation_degrees = weapon.rotation
+
+func weapon_sway(delta):
+	mouse_movement = mouse_movement.clamp(weapon.min, weapon.max)
+	position.x = lerp(position.x, weapon.position.x - (mouse_movement.x * weapon.amount_position) * delta, weapon.speed_position)
+	position.y = lerp(position.y, weapon.position.y + (mouse_movement.y * weapon.amount_position) * delta, weapon.speed_position)
+	rotation_degrees.y = lerp(rotation_degrees.y, weapon.rotation.y + (mouse_movement.x * weapon.amount_rotation) * delta, weapon.speed_rotation)
+	rotation_degrees.x = lerp(rotation_degrees.x, weapon.rotation.x - (mouse_movement.y * weapon.amount_rotation) * delta, weapon.speed_rotation)
+	
