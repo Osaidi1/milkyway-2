8e2c2b7a3e3d75c35564d9f7bbaa2ed82054066f
Osaidi1 <hassanosaid123@gmail.com>
2025-11-30 15:07:25 +0100

    idle sway almost done

--- scripts/weapon_resource.gd
+++ scripts/weapon_resource.gd
@@ -5,12 +5,16 @@ extends Resource
 @export_category("Orientation")
 @export var position: Vector3
 @export var rotation: Vector3
-@export_category("Weapon Sway")
-@export var min := Vector2(-20.0, -20.0)
-@export var max := Vector2(20.0, 20.0)
-@export_range(0, 0.2, 0.01) var speed_position := 0.07
-@export_range(0, 0.2, 0.01) var speed_rotation := 0.1
-@export_range(0, 0.25, 0.01) var amount_position := 0.1
-@export_range(0, 50, 0.1) var amount_rotation := 30.0
+@export_category("Camera Sway")
+@export var camera_min := Vector2(-20.0, -20.0)
+@export var camera_max := Vector2(20.0, 20.0)
+@export_range(0, 0.2, 0.01) var camera_speed_position := 0.07
+@export_range(0, 0.2, 0.01) var camera_speed_rotation := 0.1
+@export_range(0, 0.25, 0.01) var camera_amount_position := 0.1
+@export_range(0, 50, 0.1) var camera_amount_rotation := 30.0
+@export_category("Idle Sway")
+@export var idle_amount := 10.0
+@export var idle_strength := 300.0
+@export_range(0.1, 10.0, 0.1) var idle_random_amount := 5.0
 @export_category("Visuals")
 @export var mesh_scene: Mesh
--- scripts/weapons_manager.gd
+++ scripts/weapons_manager.gd
@@ -7,8 +7,23 @@ extends Node3D
 		weapon = value
 		if Engine.is_editor_hint():
 			load_weapon()
+@export var sway_noise: NoiseTexture2D
+@export var sway_speed := 1.2
+@export var reset := false:
+	set(value):
+		reset = value
+		if Engine.is_editor_hint():
+			load_weapon()
+
+@onready var player: CharacterBody3D = $"../../.."
 
 var mouse_movement: Vector2
+var random_sway_x: float
+var random_sway_y: float
+var random_sway_amount: float
+var time := 0.0
+var idle_sway_adjustment
+var idle_sway_rotation_strength
 
 func _ready() -> void:
 	await owner.ready
@@ -31,11 +46,30 @@ func load_weapon() -> void:
 	self.mesh = weapon.mesh_scene
 	position = weapon.position
 	rotation_degrees = weapon.rotation
+	idle_sway_adjustment = weapon.idle_amount
+	idle_sway_rotation_strength = weapon.idle_strength
+	random_sway_amount = weapon.idle_random_amount
+	
 
 func weapon_sway(delta):
-	mouse_movement = mouse_movement.clamp(weapon.min, weapon.max)
-	position.x = lerp(position.x, weapon.position.x - (mouse_movement.x * weapon.amount_position) * delta, weapon.speed_position)
-	position.y = lerp(position.y, weapon.position.y + (mouse_movement.y * weapon.amount_position) * delta, weapon.speed_position)
-	rotation_degrees.y = lerp(rotation_degrees.y, weapon.rotation.y + (mouse_movement.x * weapon.amount_rotation) * delta, weapon.speed_rotation)
-	rotation_degrees.x = lerp(rotation_degrees.x, weapon.rotation.x - (mouse_movement.y * weapon.amount_rotation) * delta, weapon.speed_rotation)
+	#Idle Sway
+	var sway_random: float = get_sway_noise()
+	var sway_random_adjusted: float = sway_random * idle_sway_adjustment
 	
+	time += delta * (sway_speed + sway_random)
+	random_sway_x = sin(time * 1.5 * sway_random_adjusted) / random_sway_amount
+	random_sway_y = sin(time - sway_random_adjusted) / random_sway_amount
+	
+	#Camera Sway
+	mouse_movement = mouse_movement.clamp(weapon.camera_min, weapon.camera_max)
+	position.x = lerp(position.x, weapon.position.x - (mouse_movement.x * weapon.camera_amount_position) * delta, weapon.camera_speed_position)
+	position.y = lerp(position.y, weapon.position.y + (mouse_movement.y * weapon.camera_amount_position) * delta, weapon.camera_speed_position)
+	rotation_degrees.y = lerp(rotation_degrees.y, weapon.rotation.y + (mouse_movement.x * weapon.camera_amount_rotation) * delta, weapon.camera_speed_rotation)
+	rotation_degrees.x = lerp(rotation_degrees.x, weapon.rotation.x - (mouse_movement.y * weapon.camera_amount_rotation) * delta, weapon.camera_speed_rotation)
+
+func get_sway_noise() -> float:
+	var player_pos := Vector3(0, 0, 0)
+	if not Engine.is_editor_hint():
+		player_pos = player.global_position
+	var noise_location := sway_noise.noise.get_noise_2d(player_pos.x, player_pos.y)
+	return noise_location
